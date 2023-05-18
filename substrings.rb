b6627b66a6e04af5378e2ad521d2d7c38b4cc9b4
Jack Sebben <jacksebben11@hotmail.com>
2023-05-18 08:19:58 -0400

    Completed. You can fill out the user_input variable to your liking. I may eventually come back for fun and make it more interactive for the shell user (lol).

--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,18 @@
-puts "first commit"
+require 'pry-byebug'
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit","slit", "disown", "grown","horny", "gorgeous", "houses","mouses","homes", "pawns"]
+user_input = "Howdy partner, sit down! How's it going?"
+
+def substrings(user_input, dictionary)
+    occurances = Hash.new
+
+    dictionary.each do |word|
+        matches = user_input.downcase.scan(word)
+        if matches.length > 0 
+            occurances[word] = matches.length
+        end 
+    end 
+    puts occurances
+end 
+
+substrings(user_input, dictionary)
+
